b42f2344dec8242b5f96b7f3f17a4b1fbada162e
RINTARO <rince@RINTAROnoiMac.local>
2020-05-27 12:26:19 +0900

    first_merge

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Tweet;
 use Illuminate\Http\Request;
 
 class MypageController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        $items = Tweet::where('user_id',$request->id);
+        return view('user.mypage',['items' => $items]);
+    }
+
+    public function search(Request $request)
+    {
+
+    }
+
+    public function delete(Request $request)
+    {
+
+    }
+
+    public function create(Request $request)
+    {
+
+    }
+
 }
--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -1,1 +1,33 @@
-<?php
+@extends('layouts.tweeter')
+
+@section('title','マイページ')
+{{--user がじぶん--}}
+{{--users他人　--}}
+@section('content')
+    @if(isset($items))
+
+        <h2>{{$thread->name}}</h2>
+
+        <ol>
+            @foreach($items as $item)
+                <li>
+                    <div>{{$item->getData()}}</div>
+                </li>
+            @endforeach
+        </ol>
+    @endif
+
+    <form action="/keijiban/thre" method="post">
+        @csrf
+        <input type="text" name="msg">
+        <input type="hidden" name="thread_id" value='{{$thread->id}}'>
+        <input type="submit" value="投稿する">
+    </form>
+    <br>
+    <a href="/keijiban">戻る</a>
+    <br>
+@endsection
+
+@section('footer')
+    copyright 2020 groupB.
+@endsection
